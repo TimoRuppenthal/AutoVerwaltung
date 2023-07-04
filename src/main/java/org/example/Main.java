f2e1b9612fc094ec5fe8ce7d4851f3d876272a01
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-07-04 08:49:15 +0200

    Anpassen des Kontextnamens, da dieser auf der Web Page nicht angezeigt wurde - In der index.html die URL angepasst - Der Main eine Greetings Methode hinzugefügt - Die Main Methode nach außen freigegeben

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,9 @@ import io.jexxa.drivingadapter.rest.RESTfulRPCAdapter;
 import java.util.List;
 
 public class Main {
+    public String AutoVerwaltung() {
+        return "AutoVerwaltung";
+    }
     @SuppressWarnings("java:S106")// Okay da Demoprojekt
     public static void main(String[] args) {
         AutoVerwaltung autoVerwaltung = new AutoVerwaltung();
@@ -24,6 +27,7 @@ public class Main {
         jexxaMain
                 // Bind a REST adapter to expose parts of the application
                 .bind(RESTfulRPCAdapter.class).to(autoVerwaltung)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
+                .bind(RESTfulRPCAdapter.class).to(Main.class)
                 .bind(RESTfulRPCAdapter.class).to(jexxaMain.getBoundedContext())  // Get stats: http://localhost:7501/BoundedContext/isRunning
 
                 // Run your application until Ctrl-C is pressed
