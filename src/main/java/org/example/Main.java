64198cc7265f85069ac413eb55f40852caa14b21
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-07-18 11:19:30 +0200

    Das Erstellen der Stammdaten (Fachlichkeit) wurde in den domainservice StammdatenService ausgelagert, damit diese nicht mehr in der Main erstellt werden.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,29 +5,21 @@ import io.jexxa.drivingadapter.rest.RESTfulRPCAdapter;
 import org.example.domain.Auto;
 import org.example.domain.AutoRepository;
 import org.example.domain.AutoVerwaltung;
+import org.example.domainservice.StammdatenService;
 import org.example.infrastructure.drivenadapter.persistence.AutoRepositoryImpl;
 
-import java.util.List;
 
 public class Main {
-    public String AutoVerwaltung() {
-        return "AutoVerwaltung";
-    }
+
 
     @SuppressWarnings("java:S106")// Okay da Demoprojekt
     public static void main(String[] args) {
         AutoRepository autoRepository = new AutoRepositoryImpl();
         AutoVerwaltung autoVerwaltung = new AutoVerwaltung(autoRepository);
-        Auto porsche =new Auto(10,5,"Porsche");
-        Auto bugatti =new Auto(22,5,"Bugatti");
-        autoVerwaltung.add(porsche);
-        autoVerwaltung.add(bugatti);
-        autoVerwaltung.delete(bugatti);
 
-        List<Auto> alleAutos = autoVerwaltung.get();
-        alleAutos.forEach( element -> System.out.println("marke: " + element.getMarke()));
-        alleAutos.forEach( element -> System.out.println("verbrauch in Liter/100Km: " + element.getVerbrauch()));
-        alleAutos.forEach( element -> System.out.println("sitzplätze: " + element.getSitzplätze()));
+        StammdatenService stammdatenService = new StammdatenService(autoRepository);
+        stammdatenService.initStammdaten();
+
         var jexxaMain = new JexxaMain(Main.class);
 
         jexxaMain
---
+++ src/main/java/org/example/domainservice/StammdatenService.java
@@ -0,0 +1,26 @@
+package org.example.domainservice;
+
+import org.example.domain.Auto;
+import org.example.domain.AutoRepository;
+
+import java.util.List;
+
+public class StammdatenService {
+    private final AutoRepository autoRepository;
+
+    public StammdatenService(AutoRepository autoRepository){
+        this.autoRepository = autoRepository;
+    }
+    public void initStammdaten(){
+        List<Auto> alleAutos = autoRepository.getAll();
+        if(!alleAutos.contains(new Auto(10, 5, "Porsche"))){
+            autoRepository.add(new Auto(10, 5, "Porsche"));
+        }
+        if(!alleAutos.contains(new Auto(22, 5, "Bugatti"))){
+            autoRepository.add(new Auto(22, 5, "Bugatti"));
+        }
+        autoRepository
+                .getAll()
+                .forEach( element -> System.out.println(element.getMarke()));
+    }
+}
