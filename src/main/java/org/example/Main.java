7d1f6f43ccbc3a59670aed34455a59d9cb8de8bb
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-07-24 09:38:39 +0200

    Umstellen von Main auf Dependency-Injection von Jexxa, damit Main keine New Aufrufe mehr macht.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,27 +2,23 @@ package org.example;
 
 import io.jexxa.core.JexxaMain;
 import io.jexxa.drivingadapter.rest.RESTfulRPCAdapter;
-import org.example.domain.AutoRepository;
+
 import org.example.applicationservice.AutoVerwaltung;
 import org.example.domainservice.StammdatenService;
 import java.util.Properties;
-import org.example.infrastructure.drivenadapter.persistence.AutoRepositoryImpl;
+
 
 public class Main {
 
     @SuppressWarnings("java:S106")// Okay da Demoprojekt
     public static void main(String[] args) {
-        AutoRepository autoRepository = new AutoRepositoryImpl(new Properties());
-        AutoVerwaltung autoVerwaltung = new AutoVerwaltung(autoRepository);
-
-        StammdatenService stammdatenService = new StammdatenService(autoRepository);
-        stammdatenService.initStammdaten();
 
         var jexxaMain = new JexxaMain(Main.class);
 
         jexxaMain
                 // Bind a REST adapter to expose parts of the application
-                .bind(RESTfulRPCAdapter.class).to(autoVerwaltung)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
+                .bootstrap(StammdatenService.class).with(StammdatenService::initStammdaten)
+                .bind(RESTfulRPCAdapter.class).to(AutoVerwaltung.class)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
                 .bind(RESTfulRPCAdapter.class).to(Main.class)
                 .bind(RESTfulRPCAdapter.class).to(jexxaMain.getBoundedContext())  // Get stats: http://localhost:7501/BoundedContext/isRunning
 
