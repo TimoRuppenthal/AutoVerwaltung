1b04b8d94440cdd2a4469bd6c4c62eb6c4dec3c8
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-11-08 09:16:24 +0100

    Methoden registriere und verifiziere der AutoVerwaltung hinzugefügt - ValueObject VerifizierungsCode hinzugefügt

--- src/main/java/org/example/applicationservice/AutoVerwaltung.java
+++ src/main/java/org/example/applicationservice/AutoVerwaltung.java
@@ -1,32 +1,32 @@
 package org.example.applicationservice;
 
 import io.jexxa.addend.applicationcore.ApplicationService;
-import org.example.domain.Auto;
-import org.example.domain.AutoRepository;
-import org.example.domain.Fahrzeugidentifikationsnummer;
-import org.example.domain.AutoDaten;
-
+import org.example.domain.*;
 import java.util.List;
 
 @ApplicationService
 public class AutoVerwaltung {
     private final AutoRepository autoRepository;
 
-    public void add(Fahrzeugidentifikationsnummer fahrzeugidentifikationsnummer, AutoDaten autoDaten){
-        autoRepository.add(new Auto(autoDaten, fahrzeugidentifikationsnummer));
+    public void add(EmailAdresse emailAdresse, AutoDaten autoDaten){
+        autoRepository.add(new Auto(autoDaten, emailAdresse));
     }
     @SuppressWarnings("unused")
-    public void delete(Fahrzeugidentifikationsnummer fahrzeugidentifikationsnummer){
-        autoRepository.remove(fahrzeugidentifikationsnummer);
+    public void delete(EmailAdresse emailAdresse){
+        autoRepository.remove(emailAdresse);
     }
 
-    public List<Fahrzeugidentifikationsnummer> get(){
+    public List<EmailAdresse> get(){
         return autoRepository.getAll()
                 .stream()
-                .map(Auto::getFahrzeugidentifikationsnummer)
+                .map(Auto::getEmailAdresse)
                 .toList();
     }
     public AutoVerwaltung (AutoRepository autoRepository){
         this.autoRepository = autoRepository;
+    }
+    public void registriere(EmailAdresse emailAdresse, AutoDaten autoDaten){
+    }
+    public void verifiziere(EmailAdresse emailAdresse, VerifizierungsCode verifizierungsCode){
     }
 }
