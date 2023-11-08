efc8a37b9bed975797c81dc1a54f9feb6ebb3b2a
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-11-08 09:54:35 +0100

    Implementierung für das Interface VerifizierungsCodeSender erstellt

--- src/main/java/org/example/domainservice/VerifizierungsCodeSender.java
+++ src/main/java/org/example/domainservice/VerifizierungsCodeSender.java
@@ -2,6 +2,7 @@ package org.example.domainservice;
 
 import io.jexxa.addend.applicationcore.InfrastructureService;
 import org.example.domain.VerifizierungsCodeVerschickt;
+import org.example.domain.EmailAdresse;
 
 public interface VerifizierungsCodeSender {
     void send(VerifizierungsCodeVerschickt verifizierungsCodeVerschickt);
