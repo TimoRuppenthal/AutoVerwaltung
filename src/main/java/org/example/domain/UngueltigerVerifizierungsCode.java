f7422ca74aa0699bfcaeb755c4d94ce2bc62da55
di50089 <timo.ruppenthal@stahl-holding-saar.de>
2023-11-08 10:32:59 +0100

    code cleanup

--- src/main/java/org/example/domain/UngueltigerVerifizierungsCode.java
+++ src/main/java/org/example/domain/UngueltigerVerifizierungsCode.java
@@ -1,6 +1,6 @@
 package org.example.domain;
 
 import io.jexxa.addend.applicationcore.BusinessException;
-
+@BusinessException
 public class UngueltigerVerifizierungsCode extends Exception {
 }
--- src/main/java/org/example/domainservice/VerifizierungsCodeSender.java
+++ src/main/java/org/example/domainservice/VerifizierungsCodeSender.java
@@ -3,7 +3,7 @@ package org.example.domainservice;
 import io.jexxa.addend.applicationcore.InfrastructureService;
 import org.example.domain.VerifizierungsCodeVerschickt;
 
-
+@InfrastructureService
 public interface VerifizierungsCodeSender {
     void send(VerifizierungsCodeVerschickt verifizierungsCodeVerschickt);
 }
